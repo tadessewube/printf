dbd909ddb75c3f7bd5da59a58611effcb4fd1cae
tadessewube <softwareteam68@gmail.com>
2022-05-17 20:32:18 +0300

    Update printf_hex_aux.c

--- printf_HEX_aux.c
+++ printf_HEX_aux.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * printf_HEX_aux - prints an hexgecimal number.
- * @num: number to print.
+ * printf_hex_aux - prints an hexgecimal number.
+ * @num: arguments.
  * Return: counter.
  */
-int printf_HEX_aux(unsigned int num)
+int printf_hex_aux(unsigned long int num)
 {
-	int i;
-	int *array;
-	int counter = 0;
-	unsigned int temp = num;
+	long int i;
+	long int *array;
+	long int counter = 0;
+	unsigned long int temp = num;
 
 	while (num / 16 != 0)
 	{
@@ -18,7 +18,7 @@ int printf_HEX_aux(unsigned int num)
 		counter++;
 	}
 	counter++;
-	array = malloc(counter * sizeof(int));
+	array = malloc(counter * sizeof(long int));
 
 	for (i = 0; i < counter; i++)
 	{
@@ -28,7 +28,7 @@ int printf_HEX_aux(unsigned int num)
 	for (i = counter - 1; i >= 0; i--)
 	{
 		if (array[i] > 9)
-			array[i] = array[i] + 7;
+			array[i] = array[i] + 39;
 		_putchar(array[i] + '0');
 	}
 	free(array);
